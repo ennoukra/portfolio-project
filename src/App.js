a89427608292519ea5bfa4fd78eb2b52190a061e
Ennoukra Abdelghani <73019287+ennoukra@users.noreply.github.com>
2021-09-09 12:58:38 +0100

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import "./bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1>hello world</h1>;
+  return <Navbar />;
 }
 
 export default App;
